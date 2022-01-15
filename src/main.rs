8c1744881a6d5a990511b102486e4390580fb40a
Pawan Dubey <pawan.dubey@shopify.com>
2022-01-15 03:57:21 -0500

    Read arguments and start prompt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use std::fmt::format;
+
+use camino::Utf8PathBuf;
+use rustyline::Editor;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt()]
+struct Options {
+    #[structopt(short, long, help = "Path to source file")]
+    file: Option<Utf8PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let options = Options::from_args();
+
+    match options.file {
+        Some(file) => run_file(&file),
+        None => run_prompt(),
+    }
+}
+
+fn run_prompt() {
+    let pkg_version = option_env!("CARGO_PKG_VERSION").unwrap_or("?");
+    let prompt = format!("(brine:{})> ", pkg_version);
+    let mut rl = Editor::<()>::new();
+
+    loop {
+        let line = rl.readline(&prompt);
+
+        match line {
+            Ok(line) => run(line),
+            Err(rustyline::error::ReadlineError::Interrupted) => break,
+            Err(rustyline::error::ReadlineError::Eof) => break,
+            Err(err) => {
+                eprintln!("Error: {}", err)
+            }
+        }
+    }
+}
+
+fn run_file(file: &Utf8PathBuf) {
+    println!("{}", file)
+}
+
+fn run(source: String) {
+    println!("{}", source)
 }
