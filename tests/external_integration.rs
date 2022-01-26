21f0cb60e9a17f6fa8709d62341c9bdf65772786
Pawan Dubey <pawan.dubey@shopify.com>
2022-01-25 23:56:53 -0500

    Write acceptance tests

---
+++ tests/external_integration.rs
@@ -0,0 +1,34 @@
+use std::{process::Command, path::PathBuf, fs};
+
+use test_case::test_case;
+
+#[test_case("chap04_scanning")]
+fn acceptance_test(chapter: &str) {
+    let binary_path_var = std::env::var("BINARY_PATH").expect("BINARY_PATH env var required");
+    let dart_dir_var = std::env::var("DART_DIR").expect("DART_DIR env var required");
+
+    let binary_path = fs::canonicalize(binary_path_var).unwrap();
+    let dart_dir = fs::canonicalize(dart_dir_var).unwrap();
+
+    let original_dir = std::env::current_dir().expect("Failed to get pwd");
+
+    std::env::set_current_dir(PathBuf::from(dart_dir)).expect("Failed to change directory");
+
+    let mut cmd = Command::new("dart");
+
+    cmd.arg("tool/bin/test.dart")
+        .arg(chapter)
+        .arg("--interpreter")
+        .arg(binary_path);
+
+    println!("Command: {:?}", cmd);
+
+    let output = cmd.output()
+        .expect("Failed to execute command");
+
+    std::env::set_current_dir(original_dir).expect("Failed to change directory");
+
+    println!("{}", String::from_utf8_lossy(&output.stdout));
+
+    assert!(output.status.success())
+}
